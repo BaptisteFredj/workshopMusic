3ea20d246258607a97ca955425774b9e11c3a85d
BaptisteFredj <redj2304@gmail.com>
2024-06-17 17:27:07 +0200

    added ArtistCard.jsx, artists.js, Nabar.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import artistsList from "./components/artists";
+import ArtistCard from "./components/ArtistCard";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <ArtistCard artists={artistsList[0]} />
+      </main>
+    </>
+  );
 }
 
 export default App;
